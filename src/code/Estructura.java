db03f57e34b67e3dd8863ea287a9513baad5c446
mreina <ma.reina.zarate@gmail.com>
2018-12-12 17:12:33 -0600

    Actualización código.

---
+++ src/code/Estructura.java
@@ -0,0 +1,18 @@
+package code;
+
+public class Estructura {
+	
+	private String cadena01, cadena02, cadena03, cadena04;
+	
+     public Estructura() {
+		// TODO Auto-generated constructor stub
+    	 this.cadena01="Tres";
+    	 this.cadena02="Tristes";
+    	 this.cadena03="Tigres";
+    	 this.cadena04="TresTistres";
+	}
+     
+     public void proceso() {
+    	 
+     }
+}
---
+++ src/code/Principal.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Código de ejemplo.
+	}
+
+}
